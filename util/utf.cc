df935c528a585bf50f7bcc25e8163857452b472f
iceboy <me@iceboy.org>
2020-03-29 16:44:49 -0700

    prettify

--- util/utf.cc
+++ util/utf.cc
@@ -41,23 +41,21 @@ size_t utf8_to_utf16_size(const char *input, size_t input_size) {
     size_t output_size = 0;
     while (input_size) {
         char c = *input;
-        if ((c & 0xc0) == 0xc0) {
-            if ((c & 0xe0) == 0xe0) {
-                if (input_size < 3) {
-                    break;
-                }
-                input += 3;
-                input_size -= 3;
-            } else {
-                if (input_size < 2) {
-                    break;
-                }
-                input += 2;
-                input_size -= 2;
-            }
-        } else {
+        if ((c & 0xc0) != 0xc0) {
             ++input;
             --input_size;
+        } else if ((c & 0xe0) != 0xe0) {
+            if (input_size < 2) {
+                break;
+            }
+            input += 2;
+            input_size -= 2;
+        } else {
+            if (input_size < 3) {
+                break;
+            }
+            input += 3;
+            input_size -= 3;
         }
         ++output_size;
     }
@@ -67,29 +65,27 @@ size_t utf8_to_utf16_size(const char *input, size_t input_size) {
 void utf8_to_utf16(const char *input, size_t input_size, char16_t *output) {
     while (input_size) {
         char c = *input;
-        if ((c & 0xc0) == 0xc0) {
-            if ((c & 0xe0) == 0xe0) {
-                if (input_size < 3) {
-                    break;
-                }
-                *output++ = (static_cast<char16_t>(c & 0x1f) << 12) |
-                            (static_cast<char16_t>(input[1] & 0x3f) << 6) |
-                            static_cast<char16_t>(input[2] & 0x3f);
-                input += 3;
-                input_size -= 3;
-            } else {
-                if (input_size < 2) {
-                    break;
-                }
-                *output++ = (static_cast<char16_t>(c & 0x3f) << 6) |
-                            static_cast<char16_t>(input[1] & 0x3f);
-                input += 2;
-                input_size -= 2;
-            }
-        } else {
+        if ((c & 0xc0) != 0xc0) {
             *output++ = static_cast<char16_t>(c & 0x7f);
             ++input;
             --input_size;
+        } else if ((c & 0xe0) != 0xe0) {
+            if (input_size < 2) {
+                break;
+            }
+            *output++ = (static_cast<char16_t>(c & 0x3f) << 6) |
+                        static_cast<char16_t>(input[1] & 0x3f);
+            input += 2;
+            input_size -= 2;
+        } else {
+            if (input_size < 3) {
+                break;
+            }
+            *output++ = (static_cast<char16_t>(c & 0x1f) << 12) |
+                        (static_cast<char16_t>(input[1] & 0x3f) << 6) |
+                        static_cast<char16_t>(input[2] & 0x3f);
+            input += 3;
+            input_size -= 3;
         }
     }
 }
