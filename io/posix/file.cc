188bcfa61a8931568d4820923123ea9da796c392
iceboy <me@iceboy.org>
2022-06-05 18:57:06 -0700

    io/posix:file add stdin, stdout and stderr

--- io/posix/file.cc
+++ io/posix/file.cc
@@ -8,6 +8,74 @@
 namespace io {
 namespace posix {
 
+SharedFile stdin(STDIN_FILENO);
+SharedFile stdout(STDOUT_FILENO);
+SharedFile stderr(STDERR_FILENO);
+
+std::error_code SharedFile::read(absl::Span<uint8_t> buffer, size_t &size) {
+    ssize_t ret = ::read(fd_, buffer.data(), buffer.size());
+    if (ret < 0) {
+        return {errno, std::system_category()};
+    }
+    size = static_cast<size_t>(ret);
+    return {};
+}
+
+std::error_code SharedFile::write(
+    absl::Span<const uint8_t> buffer, size_t &size) {
+    ssize_t ret = ::write(fd_, buffer.data(), buffer.size());
+    if (ret < 0) {
+        return {errno, std::system_category()};
+    }
+    size = static_cast<size_t>(ret);
+    return {};
+}
+
+std::error_code SharedFile::pread(
+    int64_t position, absl::Span<uint8_t> buffer, size_t &size) {
+    ssize_t ret = pread64(fd_, buffer.data(), buffer.size(), position);
+    if (ret < 0) {
+        return {errno, std::system_category()};
+    }
+    size = static_cast<size_t>(ret);
+    return {};
+}
+
+std::error_code SharedFile::pwrite(
+    int64_t position, absl::Span<const uint8_t> buffer, size_t &size) {
+    ssize_t ret = pwrite64(fd_, buffer.data(), buffer.size(), position);
+    if (ret < 0) {
+        return {errno, std::system_category()};
+    }
+    size = static_cast<size_t>(ret);
+    return {};
+}
+
+std::error_code SharedFile::seek(int64_t position) {
+    if (lseek64(fd_, position, SEEK_SET) < 0) {
+        return {errno, std::system_category()};
+    }
+    return {};
+}
+
+std::error_code SharedFile::tell(int64_t &position) {
+    off_t ret = lseek64(fd_, 0, SEEK_CUR);
+    if (ret < 0) {
+        return {errno, std::system_category()};
+    }
+    position = static_cast<int64_t>(ret);
+    return {};
+}
+
+std::error_code SharedFile::size(int64_t &size) {
+    struct stat64 stat;
+    if (fstat64(fd_, &stat) < 0) {
+        return {errno, std::system_category()};
+    }
+    size = static_cast<int64_t>(stat.st_size);
+    return {};
+}
+
 std::error_code File::open(const char *filename, int flags, mode_t mode) {
     close();
     int fd = ::open(filename, flags, mode);
@@ -24,70 +92,6 @@ void File::close() {
     }
     ::close(fd_);
     fd_ = -1;
-}
-
-std::error_code File::read(absl::Span<uint8_t> buffer, size_t &size) {
-    ssize_t ret = ::read(fd_, buffer.data(), buffer.size());
-    if (ret < 0) {
-        return {errno, std::system_category()};
-    }
-    size = static_cast<size_t>(ret);
-    return {};
-}
-
-std::error_code File::write(
-    absl::Span<const uint8_t> buffer, size_t &size) {
-    ssize_t ret = ::write(fd_, buffer.data(), buffer.size());
-    if (ret < 0) {
-        return {errno, std::system_category()};
-    }
-    size = static_cast<size_t>(ret);
-    return {};
-}
-
-std::error_code File::pread(
-    int64_t position, absl::Span<uint8_t> buffer, size_t &size) {
-    ssize_t ret = pread64(fd_, buffer.data(), buffer.size(), position);
-    if (ret < 0) {
-        return {errno, std::system_category()};
-    }
-    size = static_cast<size_t>(ret);
-    return {};
-}
-
-std::error_code File::pwrite(
-    int64_t position, absl::Span<const uint8_t> buffer, size_t &size) {
-    ssize_t ret = pwrite64(fd_, buffer.data(), buffer.size(), position);
-    if (ret < 0) {
-        return {errno, std::system_category()};
-    }
-    size = static_cast<size_t>(ret);
-    return {};
-}
-
-std::error_code File::seek(int64_t position) {
-    if (lseek64(fd_, position, SEEK_SET) < 0) {
-        return {errno, std::system_category()};
-    }
-    return {};
-}
-
-std::error_code File::tell(int64_t &position) {
-    off_t ret = lseek64(fd_, 0, SEEK_CUR);
-    if (ret < 0) {
-        return {errno, std::system_category()};
-    }
-    position = static_cast<int64_t>(ret);
-    return {};
-}
-
-std::error_code File::size(int64_t &size) {
-    struct stat64 stat;
-    if (fstat64(fd_, &stat) < 0) {
-        return {errno, std::system_category()};
-    }
-    size = static_cast<int64_t>(stat.st_size);
-    return {};
 }
 
 }  // namespace posix
--- io/posix/file.h
+++ io/posix/file.h
@@ -8,17 +8,10 @@
 namespace io {
 namespace posix {
 
-class File : public io::File {
+class SharedFile : public io::File {
 public:
-    File() : fd_(-1) {}
-    explicit File(int fd) : fd_(fd) {}
-    ~File() override { close(); }
-
-    File(const File &) = delete;
-    File &operator=(const File &) = delete;
-
-    std::error_code open(const char *filename, int flags, mode_t mode);
-    void close();
+    explicit SharedFile(int fd) : fd_(fd) {}
+    ~SharedFile() override = default;
 
     std::error_code read(absl::Span<uint8_t> buffer, size_t &size) override;
     std::error_code write(
@@ -33,8 +26,25 @@ public:
     std::error_code tell(int64_t &position) override;
     std::error_code size(int64_t &size) override;
 
-private:
+protected:
     int fd_;
+};
+
+extern SharedFile stdin;
+extern SharedFile stdout;
+extern SharedFile stderr;
+
+class File : public SharedFile {
+public:
+    File() : SharedFile(-1) {}
+    explicit File(int fd) : SharedFile(fd) {}
+    ~File() override { close(); }
+
+    File(const File &) = delete;
+    File &operator=(const File &) = delete;
+
+    std::error_code open(const char *filename, int flags, mode_t mode);
+    void close();
 };
 
 }  // namespace posix
