bfff72d20df42bde2132bfe74777ca48a9845aaf
iceboy <me@iceboy.org>
2022-06-10 16:46:06 -0700

    io:line-reader_test

---
+++ io/line-reader_test.cc
@@ -0,0 +1,31 @@
+#include "io/line-reader.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "io/memory-file.h"
+
+namespace io {
+namespace {
+
+TEST(LineReaderTest, basic) {
+    MemoryFile file({'h', 'e', 'l', 'l', 'o', '\n',
+                     'w', 'o', 'r', 'l', 'd', '\n'});
+    LineReader reader(file, {});
+    std::string line;
+    bool end_of_file;
+    std::error_code ec = reader.read(line, end_of_file);
+    ASSERT_FALSE(ec);
+    ASSERT_FALSE(end_of_file);
+    EXPECT_EQ(line, "hello\n");
+    ec = reader.read(line, end_of_file);
+    ASSERT_FALSE(ec);
+    ASSERT_FALSE(end_of_file);
+    EXPECT_EQ(line, "world\n");
+    ec = reader.read(line, end_of_file);
+    ASSERT_FALSE(ec);
+    ASSERT_TRUE(end_of_file);
+}
+
+}  // namespace
+}  // namespace io
