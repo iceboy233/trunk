3e4a40c06df3b745ff91143a52264a2f3c347edc
iceboy <me@iceboy.org>
2022-06-19 16:02:26 -0700

    io:file-utils templatize read_to_end

--- io/file-utils.cc
+++ io/file-utils.cc
@@ -19,22 +19,6 @@ std::error_code read(File &file, BufferSpan buffer, size_t &size) {
     return {};
 }
 
-std::error_code read_to_end(File &file, std::string &content) {
-    content.clear();
-    size_t pos = 0;
-    size_t size;
-    do {
-        content.resize(pos + 8192);
-        std::error_code ec = file.read({&content[pos], 8192}, size);
-        if (ec) {
-            return ec;
-        }
-        pos += size;
-        content.resize(pos);
-    } while (size);
-    return {};
-}
-
 std::error_code write(File &file, ConstBufferSpan buffer) {
     while (!buffer.empty()) {
         size_t size;
--- io/file-utils.h
+++ io/file-utils.h
@@ -13,10 +13,28 @@ namespace io {
 std::error_code read(File &file, BufferSpan buffer, size_t &size);
 
 // Reads from `file` until end of file is reached.
-std::error_code read_to_end(File &file, std::string &content);
+template <typename T, size_t buffer_size = 8192>
+std::error_code read_to_end(File &file, T &content);
 
 // Writes the entire `buffer` into `file`.
 std::error_code write(File &file, ConstBufferSpan buffer);
+
+template <typename T, size_t buffer_size>
+std::error_code read_to_end(File &file, T &content) {
+    content.clear();
+    size_t pos = 0;
+    size_t size;
+    do {
+        content.resize(pos + buffer_size);
+        std::error_code ec = file.read({&content[pos], buffer_size}, size);
+        if (ec) {
+            return ec;
+        }
+        pos += size;
+        content.resize(pos);
+    } while (size);
+    return {};
+}
 
 }  // namespace io
 
