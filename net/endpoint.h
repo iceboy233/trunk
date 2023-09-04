1e465684cc9d578deb3eaee04caa91594efb573e
iceboy <me@iceboy.org>
2023-09-03 23:17:05 -0700

    net:endpoint support ipv6

--- net/endpoint.h
+++ net/endpoint.h
@@ -8,8 +8,6 @@
 #include <string_view>
 #include <utility>
 
-#include "absl/strings/str_cat.h"
-#include "absl/strings/str_split.h"
 #include "net/asio.h"
 #include "util/strings.h"
 
@@ -35,6 +33,8 @@ public:
     std::string to_string() const;
 
 private:
+    static std::optional<AddressT> parse_address(std::string_view string);
+
     AddressT address_;
     uint16_t port_;
 };
@@ -42,23 +42,32 @@ private:
 template <typename AddressT>
 std::optional<EndpointBase<AddressT>> EndpointBase<AddressT>::from_string(
     std::string_view string) {
-    std::pair<std::string_view, std::string_view> pair =
-        absl::StrSplit(string, ':');
-    boost::system::error_code ec;
-    auto address = make_address(pair.first, ec);
-    if (ec) {
+    size_t pos = string.rfind(':');
+    if (pos == std::string_view::npos) {
         return std::nullopt;
     }
-    uint16_t port = util::consume_uint16(pair.second);
-    if (!pair.second.empty()) {
+    std::string_view address_str;
+    if (pos >= 2 && string[0] == '[' && string[pos - 1] == ']') {
+        address_str = string.substr(1, pos - 2);
+    } else {
+        address_str = string.substr(0, pos);
+    }
+    auto address_or = parse_address(address_str);
+    if (!address_or) {
         return std::nullopt;
     }
-    return EndpointBase<AddressT>(address, port);
+    std::string_view port_str = string.substr(pos + 1);
+    uint16_t port = util::consume_uint16(port_str);
+    if (!port_str.empty()) {
+        return std::nullopt;
+    }
+    return EndpointBase(*address_or, port);
 }
 
-template <typename AddressT>
-std::string EndpointBase<AddressT>::to_string() const {
-    return absl::StrCat(address_.to_string(), ":", port_);
+template <typename A, typename B>
+bool operator==(
+    const EndpointBase<A> &a, const EndpointBase<B> &b) {
+    return a.address() == b.address() && a.port() == b.port();
 }
 
 using Endpoint = EndpointBase<address>;
@@ -67,7 +76,7 @@ using EndpointV6 = EndpointBase<address_v6>;
 
 template <typename AddressT>
 bool AbslParseFlag(
-    absl::string_view in, EndpointBase<AddressT> *out, std::string *error) {
+    std::string_view in, EndpointBase<AddressT> *out, std::string *error) {
     auto address_or = EndpointBase<AddressT>::from_string(in);
     if (!address_or) {
         return false;
