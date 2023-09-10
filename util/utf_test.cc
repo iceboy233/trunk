38a33a01013a9e9309f32f3cea7f7ac465ebd15c
iceboy <me@iceboy.org>
2023-09-10 00:19:58 -0700

    util:utf_test change test fixture

--- util/utf_test.cc
+++ util/utf_test.cc
@@ -5,19 +5,20 @@
 namespace util {
 namespace {
 
+constexpr std::u16string_view utf16 =
+    u"\u3053\u3093\u306b\u3061\u306f\u3001\u4e16\u754c";
+constexpr std::string_view utf8 =
+    "\xe3\x81\x93\xe3\x82\x93\xe3\x81\xab\xe3\x81\xa1\xe3\x81\xaf\xe3\x80\x81"
+    "\xe4\xb8\x96\xe7\x95\x8c";
+
 TEST(UtfTest, utf16_to_utf8) {
     EXPECT_EQ(utf16_to_utf8(u"hello world"), "hello world");
-    EXPECT_EQ(
-        utf16_to_utf8(u"\u4f60\u597d\uff0c\u4e16\u754c"),
-        "\xe4\xbd\xa0\xe5\xa5\xbd\xef\xbc\x8c\xe4\xb8\x96\xe7\x95\x8c");
+    EXPECT_EQ(utf16_to_utf8(utf16), utf8);
 }
 
 TEST(UtfTest, utf8_to_utf16) {
     EXPECT_EQ(utf8_to_utf16("hello world"), u"hello world");
-    EXPECT_EQ(
-        utf8_to_utf16(
-            "\xe4\xbd\xa0\xe5\xa5\xbd\xef\xbc\x8c\xe4\xb8\x96\xe7\x95\x8c"),
-        u"\u4f60\u597d\uff0c\u4e16\u754c");
+    EXPECT_EQ(utf8_to_utf16(utf8), utf16);
 }
 
 }  // namespace
